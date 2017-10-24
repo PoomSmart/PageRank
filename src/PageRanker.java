ed4cd1b124fa038a5dd60724b56ac1b7502456fb
Thatchapon Unprasert <poomst@hotmail.com>
2017-10-24 20:19:41 +0700

    Code comment.

--- src/PageRanker.java
+++ src/PageRanker.java
@@ -1,3 +1,13 @@
+
+/**
+ * PageRank Project (Section 1)
+ * 
+ * 1. Kittinun Aukkapinyo	5888006
+ * 2. Chatchawan Kotarasu	5888084
+ * 3. Thatchapon Unprasert	5888220
+ * 
+ */
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
@@ -21,6 +31,12 @@ import java.util.Vector;
  */
 public class PageRanker {
 
+	/*
+	 * To simplify our data structure, we chose Node as a direct graph representation that coordinates better
+	 * relationship between nodes (or pages). Specifying in-links and out-links are therefore simple. Data structure for
+	 * those links are determined to be sets because it efficiently solves duplication issue.
+	 * 
+	 */
 	class Node {
 		public Integer pid;
 		public Set<Node> in;
@@ -42,23 +58,32 @@ public class PageRanker {
 		}
 
 		public boolean isSink() {
+			// By definition of a sink page whose out-links are none
 			return out.isEmpty();
 		}
 	}
 
-	private static final boolean debug = false;
-
+	// Conventional damping factor
 	private static final double d = 0.85;
+	// Inverse of log2 to save up computation time from time-consuming log2 division
 	private static final double ilog2 = 1.0 / Math.log(2);
 
+	// Data structure to store all pages associated with their page number
 	private Map<Integer, Node> graph;
+	// Data structure to store all sink pages
 	private Vector<Integer> S;
+	// Data structure to store PageRank scores of all pages
 	private Map<Integer, Double> PR;
+	// Integer to store floor of previous perplexity value
 	private int prevUnit;
+	// Integer to store iterations of "similar" perplexity value
 	private int iteration;
+	// Double to store perplexity value for convergence determination
 	private double perplexity;
+	// Double to cache 1/N
 	private Double iN;
 
+	// Writers and string builders for output files
 	private BufferedWriter perplexityWriter;
 	private BufferedWriter scoreWriter;
 	private StringBuilder perplexityBuilder;
@@ -78,29 +103,35 @@ public class PageRanker {
 			Integer pid;
 			StringTokenizer token;
 			graph = new HashMap<Integer, Node>();
-			if (debug)
-				System.out.println("DEBUG: File reading...");
 			while ((line = br.readLine()) != null) {
 				token = new StringTokenizer(line, " ");
+				// Always read the first page, put it into the graph if necessary
 				Node node = graph.get(pid = parseInt(token.nextToken()));
 				if (node == null)
 					graph.put(pid, node = new Node(pid));
+				// Read its in-pages, if any, put them into the graph if necessary
 				while (token.hasMoreTokens()) {
 					Node inNode = graph.get(pid = parseInt(token.nextToken()));
 					if (inNode == null)
 						graph.put(pid, inNode = new Node(pid));
+					// Out-page of all in-pages is the first page in the line
 					inNode.addOut(node);
+					// Link in-pages to the first page in the line
 					node.addIn(inNode);
 				}
 			}
-			if (debug)
-				System.out.println("DEBUG: Graph size = " + graph.size());
 			br.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
+	/**
+	 * Redefined parseInt() function that is faster than the original because it assumes seeing only positive integers.
+	 * 
+	 * @param s
+	 * @return
+	 */
 	public static Integer parseInt(final String s) {
 		final int len = s.length();
 		Integer num = '0' - s.charAt(0);
@@ -115,15 +146,16 @@ public class PageRanker {
 	 * the PageRank algorithm including setting an initial weight to each page.
 	 */
 	public void initialize() {
+		// General variables initialization
 		PR = new HashMap<Integer, Double>(graph.size());
 		S = new Vector<Integer>();
 		iN = 1.0 / graph.size();
 		for (Node node : graph.values()) {
-			PR.put(node.pid, iN);
+			PR.put(node.pid, iN); // Initial score for each page: 1/N
 			if (node.isSink())
-				S.add(node.pid);
+				S.add(node.pid); // Add sink pages here
 			else
-				node.iOutSize = 1.0 / node.out.size();
+				node.iOutSize = 1.0 / node.out.size(); // Store 1/L(q) (from pseudocode) for faster calculation
 		}
 		S.trimToSize();
 	}
@@ -133,6 +165,7 @@ public class PageRanker {
 	 * specs.
 	 */
 	public double getPerplexity() {
+		// We calculate this value in runPageRank()
 		return perplexity;
 	}
 
@@ -141,16 +174,15 @@ public class PageRanker {
 	 * PageRank algorithm continue to update the page scores).
 	 */
 	public boolean isConverge() {
+		// Each time, write out perplexity value
 		double perplexity = getPerplexity();
 		perplexityBuilder.append(perplexity);
 		perplexityBuilder.append("\n");
 		int unit = (int) perplexity;
-		if (debug) {
-			System.out.println("DEBUG: Perplexity = " + perplexity);
-			System.out.println("DEBUG: Iteration = " + iteration);
-		}
+		// When no much change in perplexity for 4 times, convergence is assumed
 		if (unit == prevUnit && ++iteration == 4)
 			return true;
+		// Otherwise, reset the counter
 		if (unit != prevUnit)
 			iteration = 1;
 		prevUnit = unit;
@@ -177,6 +209,8 @@ public class PageRanker {
 	 * 
 	 */
 	public void runPageRank(String perplexityOutFilename, String prOutFilename) {
+		// Perform the given pseudocode, with a number of optimizations to reduce as many as possible computation
+		// operations
 		double sinkPR, newPRVal, newPRVal2;
 		Map<Integer, Double> newPR = new HashMap<Integer, Double>(PR.size());
 		double dN = d * iN;
@@ -185,18 +219,17 @@ public class PageRanker {
 		perplexityBuilder = new StringBuilder();
 		scoreBuilder = new StringBuilder();
 		do {
-			if (debug)
-				System.out.println("DEBUG: Converging...");
 			sinkPR = 0.0;
 			for (Integer pid : S)
 				sinkPR += PR.get(pid);
-			newPRVal = idN + sinkPR * dN;
+			newPRVal = idN + sinkPR * dN; // This value is a starting for every page, calculated once
 			for (Node p : graph.values()) {
 				if (p.in.isEmpty())
-					newPR.put(p.pid, newPRVal);
+					newPR.put(p.pid, newPRVal); // newPRVal2 is 0, don't even need to include here
 				else {
-					newPRVal2 = 0.0;
+					newPRVal2 = 0.0; // This value is later multiplied by d
 					for (Node q : p.in)
+						// Performance: Multiplication with 1/L(q) > Division with L(q)
 						newPRVal2 += PR.get(q.pid) * q.iOutSize;
 					newPR.put(p.pid, newPRVal + d * newPRVal2);
 				}
@@ -204,12 +237,16 @@ public class PageRanker {
 			order = 0.0;
 			for (Entry<Integer, Double> entry : newPR.entrySet()) {
 				value = entry.getValue();
+				// Computing log base 2, but multiplication of log2 inverse for better performance
 				order += value * Math.log(value) * ilog2;
 				PR.put(entry.getKey(), value);
 			}
+			// TODO: Alternatively, the faster exp(-order) (hence ignoring ilog2) may be used, but precision may
+			// oscillate in a worse way
 			perplexity = Math.pow(2, -order);
 		} while (!isConverge());
 		try {
+			// Write out perplexity values and PageRank scores (PR)
 			perplexityWriter = new BufferedWriter(new FileWriter(perplexityOutFilename));
 			perplexityWriter.write(perplexityBuilder.toString());
 			perplexityWriter.close();
@@ -231,6 +268,7 @@ public class PageRanker {
 	 * Return the top K page IDs, whose scores are highest.
 	 */
 	public Integer[] getRankedPages(int K) {
+		// Wrap the graph key set with a list to be sorted using PageRank score of each page number
 		List<Integer> pages = new Vector<Integer>(graph.keySet());
 		Collections.sort(pages, new Comparator<Integer>() {
 			@Override
@@ -238,6 +276,7 @@ public class PageRanker {
 				return PR.get(p2).compareTo(PR.get(p1));
 			}
 		});
+		// Make sure |output| <= K
 		if (pages.size() > K)
 			pages = pages.subList(0, K);
 		return pages.toArray(new Integer[Math.min(K, pages.size())]);
